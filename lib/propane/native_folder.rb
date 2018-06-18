9521d1ca85334e5023b424bb11af58779460afd2
monkstone <mamba2928@yahoo.co.uk>
2018-06-18 14:40:28 +0100

    bye bye MacOS

--- lib/propane/native_folder.rb
+++ lib/propane/native_folder.rb
@@ -32,3 +32,42 @@ class NativeFolder
     '*.dylib' # MacOS
   end
 end
+
+require 'rbconfig'
+
+# Utility to load native binaries on Java CLASSPATH
+# HACK until jruby returns a more specific 'host_os' than 'linux'
+class NativeFolder
+  attr_reader :os, :bit
+
+  LINUX_FORMAT = 'linux%s'.freeze
+  ARM32 = '-armv6hf'.freeze
+  # ARM64 = '-aarch64'.freeze
+  WIN_FORMAT = 'windows%d'.freeze
+  WIN_PATTERNS = [
+    /bccwin/i,
+    /cygwin/i,
+    /djgpp/i,
+    /ming/i,
+    /mswin/i,
+    /wince/i
+  ].freeze
+
+  def initialize
+    @os = RbConfig::CONFIG['host_os'].downcase
+    @bit = java.lang.System.get_property('os.arch')
+  end
+
+  def name
+    return format(WIN_FORMAT, bit) if WIN_PATTERNS.any? { |pat| pat =~ os }
+    return format(LINUX_FORMAT, bit) if /linux/.match?(os)
+    end
+    raise 'Unsupported Architecture'
+  end
+
+  def extension
+    return '*.so' if /linux/.match?(os)
+    return '*.dll' if WIN_PATTERNS.any? { |pat| pat =~ os }
+    raise 'Unsupported Architecture'
+  end
+end
