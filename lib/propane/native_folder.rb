1a31f95dc4813fc83aa1b971a3f7a38bb4ac3209
monkstone <mamba2928@yahoo.co.uk>
2018-06-18 14:41:27 +0100

    MacOS sinks without trace

--- lib/propane/native_folder.rb
+++ lib/propane/native_folder.rb
@@ -1,41 +1,6 @@
 require 'rbconfig'
 
 # Utility to load native binaries on Java CLASSPATH
-class NativeFolder
-  attr_reader :os, :bit
-
-  WIN_FORMAT = 'windows%d'.freeze
-  LINUX_FORMAT = 'linux%d'.freeze
-  WIN_PATTERNS = [
-    /bccwin/i,
-    /cygwin/i,
-    /djgpp/i,
-    /ming/i,
-    /mswin/i,
-    /wince/i
-  ].freeze
-
-  def initialize
-    @os = RbConfig::CONFIG['host_os'].downcase
-    @bit = java.lang.System.get_property('os.arch') =~ /64/ ? 64 : 32
-  end
-
-  def name
-    return format(WIN_FORMAT, bit) if WIN_PATTERNS.any? { |pat| pat =~ os }
-    return format(LINUX_FORMAT, bit) if os =~ /linux/
-    'macosx'
-  end
-
-  def extension
-    return '*.so' if os =~ /linux/
-    return '*.dll' if WIN_PATTERNS.any? { |pat| pat =~ os }
-    '*.dylib' # MacOS
-  end
-end
-
-require 'rbconfig'
-
-# Utility to load native binaries on Java CLASSPATH
 # HACK until jruby returns a more specific 'host_os' than 'linux'
 class NativeFolder
   attr_reader :os, :bit
